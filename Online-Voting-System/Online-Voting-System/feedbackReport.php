f832d1295914914b2320e97c883df048b7c84878
ramiv <ramivr02@outlook.com>
2024-08-04 13:06:36 +0530

    hi

---
+++ Online-Voting-System/Online-Voting-System/feedbackReport.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Voting Panel</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed' rel='stylesheet' type='text/css'>
+
+    <style>
+      .headerFont{
+        font-family: 'Ubuntu', sans-serif;
+        font-size: 24px;
+      }
+
+      .subFont{
+        font-family: 'Raleway', sans-serif;
+        font-size: 14px;
+        
+      }
+      
+      .specialHead{
+        font-family: 'Oswald', sans-serif;
+      }
+
+      .normalFont{
+        font-family: 'Roboto Condensed', sans-serif;
+      }
+    </style>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    
+  <div class="container">
+    <nav class="navbar navbar-default navbar-fixed-top navbar-inverse
+    " role="navigation">
+      <div class="container">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#example-nav-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <div class="navbar-header">
+          <a href="index.html" class="navbar-brand headerFont text-lg"><strong>eVoting</strong></a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="example-nav-collapse">
+          <ul class="nav navbar-nav">
+            <!--
+             <li><a href="#featuresTab"><span class="subFont"><strong>Features</strong></span></a></li>
+            <li><a href="#feedbackTab"><span class="subFont"><strong>Feedback</strong></span></a></li>
+            <li><a href="#"><span class="subFont"><strong>About</strong></span></a></li> 
+          -->
+          </ul>
+          
+
+          <button type="submit" class="btn btn-success navbar-right navbar-btn"><span class="normalFont"><strong>Admin Panel</strong></span></button>
+        </div>
+
+      </div> <!-- end of container -->
+    </nav>
+
+    <section class="my-5">
+<div class="py-5">
+<h2 class="text-center"> contact </h2>
+</div>
+
+<div class="w-50 m-auto">
+<div class="form-group">
+<label> Username </label>
+<input type="text" name="user" autocomplete="off" class="form-control">
+
+</div>
+<div class="form-group">
+<label> Email Id </label>
+<input type="text" name="email" autocomplete="off" class="form-control">
+
+</div>
+<div class="form-group">
+<label> Mobile </label>
+<input type="text" name="mobile" autocomplete="off" class="form-control">
+
+</div>
+<div class="form-group">
+<label> Comment </label>
+<textarea class="form-control" name="comments">
+</textarea>
+
+</div>
+<center>
+<a href="index.html" class="btn btn-info"> <strong>submit</strong></a>
+</center>
+</form>
+</div>
+
+</section>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
